95332965aca3030d1bc671be141e625fb3a15a3b
Kyle Lawrence <kylelaw2814@gmail.com>
2026-05-11 11:57:17 -0400

    Added PostgreSQL support

--- app/config/settings.py
+++ app/config/settings.py
@@ -8,5 +8,9 @@ class Settings:
     APP_NAME = os.getenv("APP_NAME", "DevOps Lab API")
     ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
+    DATABASE_URL = os.getenv(
+    	"DATABASE_URL",
+    	"sqlite:///./tickets.db"
+	)
 
 settings = Settings()
--- app/database/database.py
+++ app/database/database.py
@@ -1,13 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.config.settings import settings
 
-DATABASE_URL = "sqlite:///./tickets.db"
+DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
